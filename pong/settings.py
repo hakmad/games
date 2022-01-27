4be9e17459402064db0a3c16aef2b15cfefa22fa
hakmad <88987013+hakmad@users.noreply.github.com>
2022-01-27 14:00:07 +0000

    Added docstrings and comments to settings.py for Pong game

--- pong/settings.py
+++ pong/settings.py
@@ -1,11 +1,16 @@
+"""Settings for game."""
+
+# Display settings.
 screen_width = 960
 screen_height = 540
 fps = 60
 
+# Colours.
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
+# Collision tolerance for ball and paddles.
 collision_tolerance = 10
